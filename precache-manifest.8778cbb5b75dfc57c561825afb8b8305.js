06189622c1f5462d0a689082094aa91a87d1c251
Richard Andree Ramos Benites <andree.2020@gmail.com>
2020-09-21 12:29:39 -0500

    Updates

---
+++ precache-manifest.8778cbb5b75dfc57c561825afb8b8305.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe8a0226f85b65ddd522c5832c2a9b6",
+    "url": "/richardrb-page/index.html"
+  },
+  {
+    "revision": "6e6cbcc0f2b0917ff03d",
+    "url": "/richardrb-page/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f9ce9929bd655c81784d",
+    "url": "/richardrb-page/static/css/main.a18e45f2.chunk.css"
+  },
+  {
+    "revision": "6e6cbcc0f2b0917ff03d",
+    "url": "/richardrb-page/static/js/2.7f8f6d5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/richardrb-page/static/js/2.7f8f6d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ce9929bd655c81784d",
+    "url": "/richardrb-page/static/js/main.ebebb4d7.chunk.js"
+  },
+  {
+    "revision": "b66e4e58086cbec9a023",
+    "url": "/richardrb-page/static/js/runtime-main.8d4ad357.js"
+  },
+  {
+    "revision": "aca77d73d36ed0da011e88713b7e09cb",
+    "url": "/richardrb-page/static/media/my-header.aca77d73.jpg"
+  },
+  {
+    "revision": "14f7d69698bec63178e1ede4db983d8b",
+    "url": "/richardrb-page/static/media/photo.14f7d696.png"
+  },
+  {
+    "revision": "ce03075d19d4cd6c0896216eb1cc1d88",
+    "url": "/richardrb-page/static/media/wallpaper-2.ce03075d.jpg"
+  },
+  {
+    "revision": "539f26846880709ac91aca0083943098",
+    "url": "/richardrb-page/static/media/wallpaper-3.539f2684.jpg"
+  },
+  {
+    "revision": "690cf657788ea89a9ecabc258a0388f1",
+    "url": "/richardrb-page/static/media/wallpaper.690cf657.jpg"
+  }
+]);
